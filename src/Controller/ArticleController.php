753dd3bb1c4d7130836ddc2d1d7de5f4678e4fd9
fannyberteau <fanny.berteau1@gmail.com>
2021-07-02 11:52:30 +0200

    ajout tableau

--- src/Controller/ArticleController.php
+++ src/Controller/ArticleController.php
@@ -17,10 +17,38 @@ class ArticleController {
      * @Route("/articleshow/{id}", name="articleshow")
      */
     public function articleShow($id)
-    {
-        return new Response($id);
+    { // creation du tableau
+        $articles = [
+            1 => [
+                "title" => "La vaccination c'est trop géniale",
+                "content" => "bablablblalba",
+                "id" => 1
+            ],
+            2 => [
+                "title" => "La vaccination c'est pas trop géniale",
+                "content" => "blablablabla",
+                "id" => 2
+            ],
+            3 => [
+                "title" => "Balkany c'est trop génial",
+                "content" => "balblalblalb",
+                "id" => 3
+            ],
+            4 => [
+                "title" => "Balkany c'est pas trop génial",
+                "content" => "balblalblalb",
+                "id" => 4
+            ]
+        ];
 
+        // déclaration de la variable article qui contient tous les articles défini par l'id
+        $article = $articles[$id];
+
+        // renvoie de la réponse du titre de l'article dont l'id a été préciser dans l'url
+        return new Response($article['title']);
     }
+
+
 
 }
 
